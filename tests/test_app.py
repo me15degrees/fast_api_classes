e67d233a0fd0b827a0538ea10d3b46c7f4e5ae94
me15degrees <me15degrees@gmail.com>
2024-07-08 11:36:17 -0300

    add: new testing function for html message

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,11 +4,12 @@ from fastapi.testclient import TestClient
 
 from fast_zero.app import app
 
+client = TestClient(app)
 
-def test_read_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)  # arrange
+def test_read_root_html_deve_retornar_ok_e_ola_mundo_html():
+    client = TestClient(app)  # arrange (organização)
 
-    response = client.get('/')  # act
+    response = client.get('/html')  # act (ação)
 
-    assert response.status_code == HTTPStatus.OK  # assert
-    assert response.json() == {"message": "Olá, Mundo"}  # assert
+    assert response.status_code == HTTPStatus.OK  # assert (afirmar)
+    assert response.text == '<html><body><h1>Olá Mundo!</h1></body></html>'
